3f40f970251e078b55c219a1e563e7db7f1a4a65
Gabriele Mondada <gab@ijk.ch>
2026-06-16 11:41:42 +0200

    Update dependencies URLs

--- Package.swift
+++ Package.swift
@@ -36,7 +36,7 @@ let package = Package(
             targets: ["IntegrationTestHelpers"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ml-explore/mlx-swift", .upToNextMinor(from: "0.31.3")),
+        .package(url: "https://github.com/wendylabsinc/mlx-swift", branch: "gab/demo-jetson-lm-v2"),
         .package(url: "https://github.com/swiftlang/swift-syntax.git", "600.0.0" ..< "604.0.0"),
     ],
     targets: [
